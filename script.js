c6d330460a0fedec09af4fd1a6bffb9d804c3c32
escrime13 <55107687+escrime13@users.noreply.github.com>
2020-03-10 11:07:13 -0400

    Hide/Show Travel section

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function() {
+  $("#btnAbout").on("click", function() {
+    $("#aboutMore").toggle();
+  });
+  $("#btnAboutClose").on("click", function() {
+    $("#aboutMore").toggle();
+  });
+});
