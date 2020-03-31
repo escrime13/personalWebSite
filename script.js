08edc4bbaf1233dff8c7e389ed9982409e4124a0
escrime13 <55107687+escrime13@users.noreply.github.com>
2020-03-31 16:02:36 -0400

    Changes to About Me Button and text size.

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 $(function() {
   $("#btnAbout").on("click", function() {
-    $("#aboutMore").toggle();
+    if ($("#aboutMe").css("min-height") >= "100vh") {
+      $("#aboutMe").css("min-height", "40vh");
+      $("#aboutMore").toggle();
+    } else if ($("#aboutMe").css("min-height") <= "99vh") {
+      $("#aboutMe").css("min-height", "100vh");
+      $("#aboutMore").toggle();
+    }
   });
+
   $("#btnAboutClose").on("click", function() {
     $("#aboutMore").toggle();
+    $("#aboutMe").css("min-height", "100vh");
   });
+
   $("#btnFluffy").on("click", function() {
     $("#fluffyMore").toggle();
   });
